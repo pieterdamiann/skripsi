b2f31dc82ba757671e34cac7fcbc0f40417dff13
Pieter <pieterlie0218@gmail.com>
2024-04-20 20:55:32 +0700

    upload vendor

---
+++ resources/views/login.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Login</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{('assets/css/login.css')}}" type="text/css">
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="login-page">
+            <div class="container-3">
+              <div class="container-2">
+                <div class="logo-14">
+                </div>
+                <div class="selamat-datang">
+                  Selamat Datang!
+                </div>
+              </div>
+              <div class="group-25">
+                <div class="email">
+                  Email
+                </div>
+                <div class="rectangle-4165">
+                </div>
+              </div>
+              <div class="group-24">
+                <div class="kata-sandi">
+                  Kata sandi
+                </div>
+                <div class="container-1">
+                  <div class="icons-8-hide-901">
+                  </div>
+                </div>
+              </div>
+              <p class="lupa-kata-sandi-hubungi-kami">
+                <span class="lupa-kata-sandi-hubungi-kami-sub-0"></span><span></span>
+              </p>
+              <p class="belum-punya-akun-daftar">
+                <span class="belum-punya-akun-daftar-sub-0"></span><span></span>
+              </p>
+              <div class="group-27">
+                <span class="masuk">
+                  Masuk
+                </span>
+              </div>
+              <div class="container">
+                <div class="powered-by">
+                  Powered by
+                </div>
+                <div class="binus-1">
+                </div>
+              </div>
+            </div>
+            <div class="kotak-suara-min-1">
+            </div>
+          </div>
+    </body>
+</html>
